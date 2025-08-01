3c615fb4bd8e11a122b9b00278557d890a102170
najihah <117801046+njihahhnani@users.noreply.github.com>
2025-08-01 14:49:57 +0800

    Update qr_code.py

--- qr_code.py
+++ qr_code.py
@@ -1,10 +1,34 @@
+import io
 import qrcode
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.moduledrawers.pil import RoundedModuleDrawer
+from qrcode.image.styles.colormasks import RadialGradiantColorMask
 
-website_link = "https://github.com/njihahhnani"
+website_link = input()
 
-qr = qrcode.QRCode(version=1, box_size=5, border=5)
+# Creates an QR code object and add the website link to be encoded into the QR code
+qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_H, box_size=10, border=5)
 qr.add_data(website_link)
 qr.make()
 
-img = qr.make_image(fill_color='black', back_color='white')
-img.save("github_qr.png")
+# Prints a text-based (ASCII) version of the QR code to the console
+f = io.StringIO()         # Creates a text-based in-memory file (Lives in RAM, not hard drive)
+qr.print_ascii(out=f)     # Prints the text-based (ASCII) version of the QR code to f
+f.seek(0)                 # Resets file pointer to beginning of the file f
+print(f.read())
+
+# Original QR code
+img_original = qr.make_image(fill_color='purple', back_color='white')
+img_original.save("qr_code.png")
+
+# QR code version 1
+img_1 = qr.make_image(image_factory=StyledPilImage, module_drawer=RoundedModuleDrawer())
+img_1.save("qrcode_ver1.png")
+
+# QR code version 2
+img_2 = qr.make_image(image_factory=StyledPilImage, color_mask=RadialGradiantColorMask())
+img_2.save("qrcode_ver2.png")
+
+# QR code version 3
+img_3 = qr.make_image(image_factory=StyledPilImage, embedded_image_path="./galaxy.jpg")
+img_3.save("qrcode_ver3.png")
